1f98329b08b96cb7e30af86f369d801750586361
Tunc Utku <tuncutku@Tuncs-MacBook-Pro.local>
2020-07-25 11:36:17 -0700

    add models

--- FrontEnd/pricing-service/models/alert.py
+++ FrontEnd/pricing-service/models/alert.py
@@ -1,18 +1,34 @@
+from dataclasses import dataclass, field
 from typing import List, Dict
+import uuid
+from libs.mailgun import Mailgun
 from common.database import Database
 from models.item import Item
+from models.user import User
 from models.model import Model
 
 
+@dataclass(eq=False)
 class Alert(Model):
-    collection = "alert"
+    collection: str = field(init=False, default="alerts")
+    name: str
+    item_id: str
+    price_limit: float
+    user_email: str
+    _id: str = field(default_factory=lambda: uuid.uuid4().hex)
 
-    def __init__(self, item_id: str, price_limit: float, _id: str = None):
-        super().__init__()
-        self.item_id = item_id
-        self.item = Item.get_by_id(item_id)
-        self.price_limit = price_limit
-        self._id = _id
+    def __post_init__(self):
+        self.item = Item.get_by_id(self.item_id)
+        self.user = User.find_by_email(self.user_email)
+
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "name": self.name,
+            "price_limit": self.price_limit,
+            "item_id": self.item._id,
+            "user_email": self.user_email,
+        }
 
     def load_item_price(self) -> float:
         self.item.load_price()
@@ -23,9 +39,9 @@ class Alert(Model):
             print(
                 f"Item {self.item} has reached a price under {self.price_limit}. Latest price: {self.item.price}."
             )
-
-    def save():
-        pass
-
-    def remove():
-        pass
+            Mailgun.send_email(
+                email=[self.user_email],
+                subject=f"Notification for {self.name}",
+                text=f"Your alert {self.name} has reached a price under {self.price_limit}. The latest price is {self.item.price}. Go to this address to check your item: {self.item.url}.",
+                html=f'<p>Your alert {self.name} has reached a price under {self.price_limit}.</p><p>The latest price is {self.item.price}. Check your item out <a href="{self.item.url}>here</a>.</p>',
+            )
--- FrontEnd/pricing-service/models/item.py
+++ FrontEnd/pricing-service/models/item.py
@@ -1,25 +1,21 @@
+from dataclasses import dataclass, field
 from typing import Dict, List
 from bs4 import BeautifulSoup
 import requests
 import re
-
-from models.model import Model
+import uuid
 from common.database import Database
+from models.model import Model
 
 
+@dataclass(eq=False)
 class Item(Model):
-    collection = "item"
-
-    def __init__(self, url: str, tag_name: str, query: Dict, _id: str = None):
-        super().__init__()
-        self.url = url
-        self.tag_name = tag_name
-        self.query = query
-        self.price = None
-        self._id = _id
-
-    def __repr__(self):
-        return f"<Item {self.url}>"
+    collection: str = field(init=False, default="items")
+    url: str
+    tag_name: str
+    query: Dict
+    price: float = field(default=None)
+    _id: str = field(default_factory=lambda: uuid.uuid4().hex)
 
     def load_price(self) -> float:
         request = requests.get(self.url)
@@ -35,8 +31,11 @@ class Item(Model):
         self.price = float(without_commas)
         return self.price
 
-    def save(self) -> None:
-        Database.insert_item(self.url, self.tag_name, self.query)
-
-    def remove():
-        pass
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "url": self.url,
+            "tag_name": self.tag_name,
+            "price": self.price,
+            "query": self.query
+        }
